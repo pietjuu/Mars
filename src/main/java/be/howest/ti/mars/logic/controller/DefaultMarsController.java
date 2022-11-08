673f3d0a08ef965279f6dc06bc999ab2c0a27954
glenn.callens <glenn.callens@student.howest.be>
2022-11-08 14:19:49 +0100

    :sparkles: add createUser

--- src/main/java/be/howest/ti/mars/logic/controller/DefaultMarsController.java
+++ src/main/java/be/howest/ti/mars/logic/controller/DefaultMarsController.java
@@ -1,9 +1,15 @@
 package be.howest.ti.mars.logic.controller;
 
+import be.howest.ti.mars.logic.data.Repositories;
+import be.howest.ti.mars.logic.domain.users.PricePlan;
+import be.howest.ti.mars.logic.domain.users.User;
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.NoSuchElementException;
+
 /**
  * DefaultMarsController is the default implementation for the MarsController interface.
  * The controller shouldn't even know that it is used in an API context..
- *
  * This class and all other classes in the logic-package (or future sub-packages)
  * should use 100% plain old Java Objects (POJOs). The use of Json, JsonObject or
  * Strings that contain encoded/json data should be avoided here.
@@ -12,4 +18,21 @@ package be.howest.ti.mars.logic.controller;
  */
 public class DefaultMarsController implements MarsController {
 
+    @Override
+    public void createUser(String firstname, String lastname, String subscription) {
+
+        if (StringUtils.isBlank(firstname) || StringUtils.isBlank(lastname) || StringUtils.isBlank(subscription)){
+            throw new IllegalArgumentException("An empty argument is not allowed!");
+        }
+
+        try {
+
+            PricePlan pricePlan = PricePlan.valueOf(subscription);
+            User user = new User(firstname, lastname, pricePlan);
+            Repositories.getInMemoryRepository().addUser(user);
+
+        } catch (IllegalArgumentException e){
+            throw new NoSuchElementException(String.format("No such element %s", subscription));
+        }
+    }
 }
--- src/main/java/be/howest/ti/mars/logic/controller/MarsController.java
+++ src/main/java/be/howest/ti/mars/logic/controller/MarsController.java
@@ -1,5 +1,5 @@
 package be.howest.ti.mars.logic.controller;
 
 public interface MarsController {
-
+    void createUser(String firstname, String lastname, String pricePlan);
 }
