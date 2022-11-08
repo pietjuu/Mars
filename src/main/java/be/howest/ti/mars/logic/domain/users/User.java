62cf6ddfb7b940c459adab4a3ac414a26ad1c469
glenn.callens <glenn.callens@student.howest.be>
2022-11-08 11:49:33 +0100

    :construction: add getter / setters user

--- src/main/java/be/howest/ti/mars/logic/domain/users/User.java
+++ src/main/java/be/howest/ti/mars/logic/domain/users/User.java
@@ -1,23 +1,57 @@
 package be.howest.ti.mars.logic.domain.users;
 
+import java.util.UUID;
+
 public class User {
 
-    private String id;
-    private String firstname;
-    private String lastname;
-    private Priceplan priceplan;
+    private final String id;
+    private final String firstname;
+    private final String lastname;
+    private PricePlan priceplan;
 
-    public User(String id, String firstname, String lastname, Priceplan priceplan) {
+    /**
+     * User object - Constructor should only be used with DB.
+     * @param id id of user
+     * @param firstname firstname
+     * @param lastname lastname
+     * @param pricePlan PricePlan enum
+     */
+    public User(String id, String firstname, String lastname, PricePlan pricePlan) {
         this.id = id;
         this.firstname = firstname;
         this.lastname = lastname;
-        this.priceplan = priceplan;
+        this.priceplan = pricePlan;
     }
 
-    public User(String id, String firstname, String lastname) {
-        this.id = id;
+    /**
+     * Constructor
+     * @param firstname firstname
+     * @param lastname lastname
+     */
+    public User(String firstname, String lastname) {
+        this.id = UUID.randomUUID().toString();
         this.firstname = firstname;
         this.lastname = lastname;
-        this.priceplan = Priceplan.STANDARD;
+        this.priceplan = PricePlan.STANDARD;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public PricePlan getPricePlan() {
+        return priceplan;
+    }
+
+    public void setPricePlan(PricePlan priceplan) {
+        this.priceplan = priceplan;
     }
 }
