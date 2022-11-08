85b007efb2ae1bd505847d5f9a090301fb334fae
glenn.callens <glenn.callens@student.howest.be>
2022-11-08 14:20:28 +0100

    :card_file_box: add addUser()

--- src/main/java/be/howest/ti/mars/logic/data/InMemoryRepository.java
+++ src/main/java/be/howest/ti/mars/logic/data/InMemoryRepository.java
@@ -1,7 +1,16 @@
 package be.howest.ti.mars.logic.data;
 
-public class InMemoryRepository {
+import be.howest.ti.mars.logic.domain.users.User;
 
+import java.util.HashSet;
+import java.util.Set;
 
+public class InMemoryRepository implements MarsRepositories{
 
+    Set<User> users = new HashSet<>();
+
+    @Override
+    public void addUser(User user) {
+        users.add(user);
+    }
 }
