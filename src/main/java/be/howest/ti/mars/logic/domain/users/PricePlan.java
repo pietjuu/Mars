e54873a50eadf2369316efbfedadb9c141395337
glenn.callens <glenn.callens@student.howest.be>
2022-11-08 11:45:06 +0100

    :truck: rename to PricePlan

--- src/main/java/be/howest/ti/mars/logic/domain/users/PricePlan.java
+++ src/main/java/be/howest/ti/mars/logic/domain/users/PricePlan.java
@@ -1,6 +1,6 @@
 package be.howest.ti.mars.logic.domain.users;
 
-public enum Priceplan {
+public enum PricePlan {
 
     STANDARD("Standard", 10, 5),
     PREMIUM("Premium", 100, -1),
@@ -10,7 +10,7 @@ public enum Priceplan {
     private final int maxItems;
     private final double costPerMol;
 
-    Priceplan(String name, int maxItems, double costPerMol){
+    PricePlan(String name, int maxItems, double costPerMol){
         this.name = name;
         this.maxItems = maxItems;
         this.costPerMol = costPerMol;
