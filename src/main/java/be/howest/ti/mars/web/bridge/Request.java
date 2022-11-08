f227c492e1cdd7117f9023941240b9786eeca1dc
glenn.callens <glenn.callens@student.howest.be>
2022-11-08 14:24:01 +0100

    :construction: add functions for getting parameters firstname, lastname, subscription out of body.

--- src/main/java/be/howest/ti/mars/web/bridge/Request.java
+++ src/main/java/be/howest/ti/mars/web/bridge/Request.java
@@ -11,7 +11,6 @@ import java.util.logging.Logger;
 /**
  * The Request class is responsible for translating information that is part of the
  * request into Java.
- *
  * For every piece of information that you need from the request, you should provide a method here.
  * You can find information in:
  * - the request path: params.pathParameter("some-param-name")
@@ -19,18 +18,15 @@ import java.util.logging.Logger;
  * Both return a `RequestParameter`, which can contain a string or an integer in our case.
  * The actual data can be retrieved using `getInteger()` or `getString()`, respectively.
  * You can check if it is an integer (or not) using `isNumber()`.
- *
  * Finally, some requests have a body. If present, the body will always be in the json format.
  * You can acces this body using: `params.body().getJsonObject()`.
- *
  * **TIP:** Make sure that al your methods have a unique name. For instance, there is a request
  * that consists of more than one "player name". You cannot use the method `getPlayerName()` for both,
  * you will need a second one with a different name.
  */
 public class Request {
+    public static final String ERROR_BODY = "Unable to decipher the data in the request body. See logs for details.";
     private static final Logger LOGGER = Logger.getLogger(Request.class.getName());
-    public static final String SPEC_QUOTE_ID = "quoteId";
-    public static final String SPEC_QUOTE = "quote";
     private final RequestParameters params;
 
     public static Request from(RoutingContext ctx) {
@@ -40,4 +36,38 @@ public class Request {
     private Request(RoutingContext ctx) {
         this.params = ctx.get(ValidationHandler.REQUEST_CONTEXT_KEY);
     }
+
+    public String getUserFirstname() {
+        try {
+            if (params.body().isJsonObject())
+                return params.body().getJsonObject().getString("firstname");
+            return params.body().get().toString();
+        } catch (IllegalArgumentException ex) {
+            LOGGER.log(Level.INFO, "Unable to decipher 'firstname' in the body of POST user", ex);
+            throw new MalformedRequestException(ERROR_BODY);
+        }
+    }
+
+    public String getUserLastname() {
+        try {
+            if (params.body().isJsonObject())
+                return params.body().getJsonObject().getString("lastname");
+            return params.body().get().toString();
+        } catch (IllegalArgumentException ex) {
+            LOGGER.log(Level.INFO, "Unable to decipher 'lastname' in the body of POST user", ex);
+            throw new MalformedRequestException(ERROR_BODY);
+        }
+    }
+
+    public String getUserPricePlan(){
+        try {
+            if (params.body().isJsonObject())
+                return params.body().getJsonObject().getString("subscription");
+            return params.body().get().toString();
+        } catch (IllegalArgumentException ex){
+            LOGGER.log(Level.INFO, "Unable to decipher 'subscription' in the body of POST user", ex);
+            throw new MalformedRequestException(ERROR_BODY);
+        }
+    }
+
 }
