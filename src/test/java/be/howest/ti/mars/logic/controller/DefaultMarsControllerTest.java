11f819509bf6b5e00e6e6750afa8f102f9b545bd
glenn.callens <glenn.callens@student.howest.be>
2022-11-09 10:47:15 +0100

    :white_check_mark: add testCreateUser()

--- src/test/java/be/howest/ti/mars/logic/controller/DefaultMarsControllerTest.java
+++ src/test/java/be/howest/ti/mars/logic/controller/DefaultMarsControllerTest.java
@@ -1,25 +1,16 @@
 package be.howest.ti.mars.logic.controller;
 
-import be.howest.ti.mars.logic.data.Repositories;
-import be.howest.ti.mars.logic.domain.users.User;
-import io.vertx.core.json.JsonObject;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
-
-import java.util.Map;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class DefaultMarsControllerTest {
-
-    private static final String URL = "jdbc:h2:./db-05";
-
     @Test
-    void testCreateUser(String firstname, String lastname, String subscription){
+    void testCreateUser(){
         MockMarsController controller = new MockMarsController();
 
-        controller.createUser("Glenn", "Callens", "FREE");
+        assertEquals("Glenn", controller.createUser("Glenn", "Callens", "PREMIUM").getFirstname());
 
     }
 
