80316871d40637b5d77d1274d7bd02984f66820f
glenn.callens <glenn.callens@student.howest.be>
2022-11-09 12:05:25 +0100

    :construction: add hashcode,equal , ...

--- src/main/java/be/howest/ti/mars/logic/domain/users/User.java
+++ src/main/java/be/howest/ti/mars/logic/domain/users/User.java
@@ -42,4 +42,17 @@ public class User extends BaseUser{
     public String toString() {
         return this.getFirstname() + " " + this.getLastname();
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        BaseUser baseUser = (BaseUser) o;
+        return this.getId().equals(baseUser.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(this.getId());
+    }
 }
