e497a1fb1bda9958665f8a7586e4aa14f4f92778
glenn.callens <glenn.callens@student.howest.be>
2022-11-09 10:36:42 +0100

    :card_file_box: Add getUsers and dummydata

--- src/main/java/be/howest/ti/mars/logic/data/InMemoryRepository.java
+++ src/main/java/be/howest/ti/mars/logic/data/InMemoryRepository.java
@@ -1,5 +1,6 @@
 package be.howest.ti.mars.logic.data;
 
+import be.howest.ti.mars.logic.domain.users.PricePlan;
 import be.howest.ti.mars.logic.domain.users.User;
 
 import java.util.HashSet;
@@ -9,8 +10,21 @@ public class InMemoryRepository implements MarsRepositories{
 
     Set<User> users = new HashSet<>();
 
+    public InMemoryRepository(){
+        addUser(new User("Thibo", "Verbeerst", PricePlan.BUSINESS));
+        addUser(new User("Pieter", "Verheye", PricePlan.PREMIUM));
+        addUser(new User("Delia", "Vervaeke", PricePlan.STANDARD));
+        addUser(new User("Wiebe", "Desmadryl", PricePlan.STANDARD));
+        addUser(new User("Glenn", "Callens", PricePlan.PREMIUM));
+    }
+
     @Override
     public void addUser(User user) {
         users.add(user);
+    }
+
+    @Override
+    public Set<User> getUsers() {
+        return users;
     }
 }
--- src/main/java/be/howest/ti/mars/logic/data/MarsRepositories.java
+++ src/main/java/be/howest/ti/mars/logic/data/MarsRepositories.java
@@ -2,8 +2,11 @@ package be.howest.ti.mars.logic.data;
 
 import be.howest.ti.mars.logic.domain.users.User;
 
+import java.util.Set;
+
 public interface MarsRepositories {
 
     void addUser(User user);
 
+    Set<User> getUsers();
 }
