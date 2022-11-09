2569cdbe809032a0711fc5c8329690f3a162e5df
Thibo Verbeerst <thibo.verbeerst@student.howest.be>
2022-11-09 10:12:28 +0100

    set colors for white theme

---
+++ src/components/Navigation/SideNavigation.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "SideNavigation"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/layouts/LoggedInLayout.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  name: "BaseLayout"
+}
+</script>
+
+<style scoped>
+
+</style>
