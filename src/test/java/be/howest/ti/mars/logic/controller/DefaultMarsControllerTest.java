cb339a6c41839b8e19deedd2fa2ba4b1fa982a67
glenn.callens <glenn.callens@student.howest.be>
2022-11-09 10:50:27 +0100

    :white_check_mark: add testGetUsers()

--- src/test/java/be/howest/ti/mars/logic/controller/DefaultMarsControllerTest.java
+++ src/test/java/be/howest/ti/mars/logic/controller/DefaultMarsControllerTest.java
@@ -1,7 +1,12 @@
 package be.howest.ti.mars.logic.controller;
 
+import be.howest.ti.mars.logic.domain.users.BaseUser;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
@@ -12,6 +17,16 @@ class DefaultMarsControllerTest {
 
         assertEquals("Glenn", controller.createUser("Glenn", "Callens", "PREMIUM").getFirstname());
 
+    }
+
+    @Test
+    void testGetUsers(){
+        MockMarsController controller = new MockMarsController();
+
+        assertEquals(2, controller.getUsers().size());
+        List<BaseUser> list = new ArrayList<>(controller.getUsers());
+        assertEquals("Thibo", list.get(0).getFirstname());
+        assertEquals("Glenn", list.get(1).getFirstname());
     }
 
 }
