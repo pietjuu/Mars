57fcffe5cbffe73c3b7540152bff6198a60e9eac
glenn.callens <glenn.callens@student.howest.be>
2022-11-09 12:05:10 +0100

    :white_check_mark: add UserTest

---
+++ src/test/java/be/howest/ti/mars/logic/domain/users/UserTest.java
@@ -0,0 +1,26 @@
+package be.howest.ti.mars.logic.domain.users;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserTest {
+
+    @Test
+    void testCreateUser(){
+        User u1 = new User("Glenn", "Callens", PricePlan.STANDARD);
+
+        assertNotNull(u1.getId());
+        assertEquals(PricePlan.STANDARD, u1.getPricePlan());
+        u1.setPricePlan(PricePlan.PREMIUM);
+        assertEquals(PricePlan.PREMIUM, u1.getPricePlan());
+    }
+
+    @Test
+    void testEqual(){
+        User u1 = new User("1", "Glenn", "Callens", PricePlan.STANDARD);
+        User u2 = new User("1","Glenn", "Callens", PricePlan.STANDARD);
+
+        assertEquals(u1, u2);
+    }
+}
