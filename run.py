f1ed36f5a1ad30c4dc879e1f82822fc9a9ceeba2
pieter <pieter.verheye@student.howest.be>
2022-11-09 12:17:16 +0000

    make test folder with test code for RPI

--- run.py
+++ run.py
@@ -2,4 +2,4 @@ from transporter import app
 
 
 if __name__ == '__main__':
-    app.run(debug=True) # host="0.0.0.0"
+    app.run(host="0.0.0.0") # host="0.0.0.0"
---
+++ test/testLED.py
@@ -0,0 +1,33 @@
+#Libraries
+import RPi.GPIO as GPIO
+from time import sleep
+#Set warnings off (optional)
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+
+#Set Button and LED pins
+Button = 23
+LED = 24
+
+#Setup Button and LED
+GPIO.setup(Button,GPIO.IN,pull_up_down=GPIO.PUD_UP)
+GPIO.setup(LED,GPIO.OUT)
+#flag = 0
+
+'''
+while True:
+	button_state = GPIO.input(Button)
+	sleep(1)
+	print(button_state)
+'''
+
+while True:
+    button_state = GPIO.input(Button)
+    print(button_state)
+    if button_state == 0:
+        GPIO.output(LED,GPIO.HIGH)
+        print("HIGH")
+    else:
+        GPIO.output(LED,GPIO.LOW)
+        print("LOW")
+    sleep(1)
