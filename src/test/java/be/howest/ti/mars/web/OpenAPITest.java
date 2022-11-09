e14ce4fa0b973f556a7076b0f050f8aa86694b9b
glenn.callens <glenn.callens@student.howest.be>
2022-11-09 11:53:20 +0100

    :white_check_mark: add createUser()

--- src/test/java/be/howest/ti/mars/web/OpenAPITest.java
+++ src/test/java/be/howest/ti/mars/web/OpenAPITest.java
@@ -5,6 +5,7 @@ import be.howest.ti.mars.logic.data.Repositories;
 import be.howest.ti.mars.web.bridge.MarsOpenApiBridge;
 import be.howest.ti.mars.web.bridge.MarsRtcBridge;
 import io.vertx.core.Vertx;
+import io.vertx.core.json.JsonObject;
 import io.vertx.ext.web.client.WebClient;
 import io.vertx.junit5.VertxExtension;
 import io.vertx.junit5.VertxTestContext;
@@ -29,7 +30,7 @@ class OpenAPITest {
     private static final int PORT = 8080;
     private static final String HOST = "localhost";
     public static final String MSG_200_EXPECTED = "If all goes right, we expect a 200 status";
-    //public static final String MSG_201_EXPECTED = "If a resource is successfully created.";
+    public static final String MSG_201_EXPECTED = "If a resource is successfully created.";
     //public static final String MSG_204_EXPECTED = "If a resource is successfully deleted";
     private Vertx vertx;
     private WebClient webClient;
@@ -80,5 +81,25 @@ class OpenAPITest {
                     assertTrue(stringList.contains("Glenn"));
                     testContext.completeNow();
                 }));
+    }
+
+    @Test
+    void createUsers(final VertxTestContext testContext){
+        webClient.post(PORT, HOST, "/api/users").sendJsonObject(createUser())
+                .onFailure(testContext::failNow)
+                .onSuccess(response -> testContext.verify(() -> {
+                    assertEquals(201, response.statusCode(), MSG_201_EXPECTED);
+                    assertEquals("testID01", response.bodyAsJsonObject().getString("id"));
+                    testContext.completeNow();
+                }));
+    }
+
+    private JsonObject createUser(){
+        JsonObject jsonObject = new JsonObject();
+        jsonObject.put("firstname", "Bob");
+        jsonObject.put("lastname", "Bob");
+        jsonObject.put("subscription", "STANDARD");
+
+        return jsonObject;
     }
 }
