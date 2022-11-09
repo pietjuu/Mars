152ec290cb5814c37f5a55818c834362f13f4130
pieter <pieter.verheye@student.howest.be>
2022-11-09 13:59:08 +0000

    yellow lights blinks when pushbutton isn't pressed

--- test/test_pushbutton.py
+++ test/test_pushbutton.py
@@ -1,4 +1,4 @@
-#Libaries 
+#Libaries
 import RPi.GPIO as GPIO
 from time import sleep
 
@@ -9,12 +9,19 @@ GPIO.setmode(GPIO.BCM)
 #Set pins to button
 Button = 23
 
-#Setup button
+#Set pins to LED
+Led_door_closed = 24
+Led_door_open = 25
 
+#Setup button
 GPIO.setup(Button, GPIO.IN)
+GPIO.setup(Led_door_closed,GPIO.OUT)
+GPIO.setup(Led_door_open,GPIO.OUT)
 
 #Logic program
 while True:
 	button_state = GPIO.input(Button)
 	print(button_state)
-
+	if button_state == 0:
+		GPIO.output(Led_door_closed, GPIO.LOW)
+		GPIO.output(Led_door_open, GPIO.HIGH)
