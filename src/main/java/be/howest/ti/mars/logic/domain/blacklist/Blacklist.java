8fe79a28e7edb2d4d0d9438f1264e16e5b97fb58
glenn.callens <glenn.callens@student.howest.be>
2022-11-10 13:56:46 +0100

    :construction: update for use Blacklist as Shippert's blacklist

--- src/main/java/be/howest/ti/mars/logic/domain/blacklist/Blacklist.java
+++ src/main/java/be/howest/ti/mars/logic/domain/blacklist/Blacklist.java
@@ -3,14 +3,24 @@ package be.howest.ti.mars.logic.domain.blacklist;
 import be.howest.ti.mars.logic.domain.items.Item;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Blacklist {
 
-    private final List<Item> list;
+    private final Set<Item> list;
 
     public Blacklist(){
-        this.list = new ArrayList<>();
+        this.list = new HashSet<>();
+    }
+
+    public List<Item> getItems(){
+        return new ArrayList<>(list);
+    }
+
+    public void addItem(Item item){
+        list.add(item);
     }
 
 }
