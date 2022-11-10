db7a86bb0dbf8caa7a08266a2e7260cd40fe8dc4
pieter <pieter.verheye@student.howest.be>
2022-11-10 14:00:18 +0000

    LCD screen print hello world works

---
+++ test/LCD_test.py
@@ -0,0 +1,28 @@
+# Import LCD library
+from RPLCD import i2c
+
+# Import sleep library
+from time import sleep
+
+# constants to initialise the LCD
+lcdmode = 'i2c'
+cols = 15
+rows = 2
+charmap = 'A00'
+i2c_expander = 'PCF8574'
+
+# Generally 27 is the address;Find yours using: i2cdetect -y 1 
+address = 0x3f
+port = 1 # 0 on an older Raspberry Pi
+
+# Initialise the LCD
+lcd = i2c.CharLCD(i2c_expander, address, port=port, charmap=charmap,
+                  cols=cols, rows=rows)
+
+# Write a string on first line and move to next line
+lcd.write_string('Hello world')
+sleep(5)
+#Switch off backlight
+lcd.backlight_enabled = False 
+#Clear the LCD screen
+lcd.close(clear=True)
