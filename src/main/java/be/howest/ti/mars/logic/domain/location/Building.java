d3b72d26067a2c9b1fe928d755036c4fd6238d6e
glenn.callens <glenn.callens@student.howest.be>
2022-11-10 10:32:02 +0100

    :construction: implement coordinates and add getters

--- src/main/java/be/howest/ti/mars/logic/domain/location/Building.java
+++ src/main/java/be/howest/ti/mars/logic/domain/location/Building.java
@@ -6,15 +6,29 @@ public class Building {
 
     private final String id;
     private final TypeOfLocation typeOfLocation;
+    private final Coordinates coordinates;
 
-    public Building(TypeOfLocation typeOfLocation){
+    public Building(TypeOfLocation typeOfLocation, Coordinates coordinates){
         this.id = UUID.randomUUID().toString();
         this.typeOfLocation = typeOfLocation;
+        this.coordinates = coordinates;
     }
 
-    public Building(String id, TypeOfLocation typeOfLocation){
+    public Building(String id, TypeOfLocation typeOfLocation , Coordinates coordinates){
         this.id = id;
         this.typeOfLocation = typeOfLocation;
+        this.coordinates = coordinates;
     }
 
+    public String getId() {
+        return id;
+    }
+
+    public TypeOfLocation getTypeOfLocation() {
+        return typeOfLocation;
+    }
+
+    public Coordinates getCoordinates() {
+        return coordinates;
+    }
 }
