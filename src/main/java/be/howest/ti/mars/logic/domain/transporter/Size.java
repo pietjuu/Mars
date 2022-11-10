692388855563a4b20cf087290043fcd28ab016af
glenn.callens <glenn.callens@student.howest.be>
2022-11-10 13:03:42 +0100

    :sparkles: add Size

---
+++ src/main/java/be/howest/ti/mars/logic/domain/transporter/Size.java
@@ -0,0 +1,16 @@
+package be.howest.ti.mars.logic.domain.transporter;
+
+public class Size {
+
+    private double height;
+    private double length;
+    private double width;
+
+    public Size(double height, double length, double width) {
+        this.height = height;
+        this.length = length;
+        this.width = width;
+    }
+
+
+}
