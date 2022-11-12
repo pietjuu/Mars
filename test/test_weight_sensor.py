e77f56bbe69c7cb3648a89a50d03fa50b18146e4
pieter <pieter.verheye@student.howest.be>
2022-11-12 10:49:10 +0000

    fix hx.reset + get data (i think bits from sensor)

--- test/test_weight_sensor.py
+++ test/test_weight_sensor.py
@@ -11,11 +11,21 @@ GPIO.setmode(GPIO.BCM)
 hx = HX711(dout_pin=5, pd_sck_pin=6)
 
 #Reset HX711
-reset = hx.reset()
-print(reset)
-if not reset:
+print("dit is de hx", hx)
+err = hx.reset()
+print("dit is de reset", err)
+if not err == False:
 	print("something went wrong")
 	print("if statement werkt, tot hiertoe werkt alles")
 else:
 	print("klaar om te gebruiken")
 	print("else statement werkt, tot hiertoe werkt alles")
+
+
+reading = hx.get_raw_data_mean()
+print (reading)
+if reading:
+	print('Data subtracted by offset but still not converted to units:', reading)
+else:
+	print('invalid data', reading)
+
