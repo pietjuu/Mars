6f82784483434bc875666b84a34f825a46b1b7fe
Thibo Verbeerst <thibo.verbeerst@student.howest.be>
2022-11-12 11:23:59 +0100

    redirect to /dashboard

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 
 import DashboardView from "@/views/DashboardView";
 import SendItemView from "@/views/SendItemView";
@@ -8,6 +8,10 @@ import HistoryView from "@/views/HistoryView";
 import BlacklistView from "@/views/BlacklistView";
 
 const routes = [
+  {
+    path: '/',
+    redirect: '/dashboard'
+  },
   {
     path: '/dashboard',
     name: 'Dashboard',
@@ -46,7 +50,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(),
   routes
 })
 
