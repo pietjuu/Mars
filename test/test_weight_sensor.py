6a95cfa7d61f45807bdc97635c5f201f664d774d
pieter <pieter.verheye@student.howest.be>
2022-11-12 11:00:55 +0000

    code checks if there is error and after calibration it prints a value in grams

--- test/test_weight_sensor.py
+++ test/test_weight_sensor.py
@@ -21,7 +21,7 @@ else:
 	print("klaar om te gebruiken")
 	print("else statement werkt, tot hiertoe werkt alles")
 
-
+# Still testing this (gets bits)
 reading = hx.get_raw_data_mean()
 print (reading)
 if reading:
@@ -29,3 +29,25 @@ if reading:
 else:
 	print('invalid data', reading)
 
+# In order to calculate the conversion ratio to some units, in my case I want grams,
+# you must have known weight.
+input('Put known weight on the scale and then press Enter')
+reading = hx.get_data_mean()
+if reading:
+	print('Mean value from HX711 subtracted by offset:', reading)
+	known_weight_grams = input('Write how many grams it was and press Enter: ')
+	try:
+		value = float(known_weight_grams)
+		print(value, 'grams')
+	except ValueError:
+		print('Expected integer or float and I have got:', known_weight_grams)
+
+
+# Read data several times and return mean value
+# subtracted by offset and converted by scale ratio to
+# desired units. In my case in grams.
+print("Now, I will read data in infinite loop. To exit press 'CTRL + C'")
+input('Press Enter to begin reading')
+print('Current weight on the scale in grams is: ')
+while True:
+	print(hx.get_weight_mean(20), 'g')
