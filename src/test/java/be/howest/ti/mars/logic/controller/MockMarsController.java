34a0db028d157b30bec108ae5548529e1479a8d8
glenn.callens <glenn.callens@student.howest.be>
2022-11-17 10:38:22 +0100

    :construction: add blacklists functions

--- src/test/java/be/howest/ti/mars/logic/controller/MockMarsController.java
+++ src/test/java/be/howest/ti/mars/logic/controller/MockMarsController.java
@@ -1,5 +1,8 @@
 package be.howest.ti.mars.logic.controller;
 
+import be.howest.ti.mars.logic.domain.blacklist.Blacklist;
+import be.howest.ti.mars.logic.domain.blacklist.UserBlacklist;
+import be.howest.ti.mars.logic.domain.items.Item;
 import be.howest.ti.mars.logic.domain.users.BaseUser;
 import be.howest.ti.mars.logic.domain.users.PricePlan;
 import be.howest.ti.mars.logic.domain.users.User;
@@ -37,5 +40,35 @@ public class MockMarsController implements MarsController {
 
     @Override
     public void deleteUser(String userID) {
+    }
+
+    @Override
+    public Blacklist getShippertBlacklist() {
+        Blacklist blacklist = new Blacklist();
+        blacklist.addItem(new Item("Apple"));
+
+        return blacklist;
+    }
+
+    @Override
+    public UserBlacklist getUserBlacklist(String userID) {
+        UserBlacklist userBlacklist = new UserBlacklist(userID);
+        userBlacklist.addItem(new Item("Bananas"));
+
+        return userBlacklist;
+    }
+
+    @Override
+    public void addItemToUserBlacklist(String itemName, String userID) {
+        UserBlacklist userBlacklist = new UserBlacklist(userID);
+        userBlacklist.addItem(new Item(itemName));
+    }
+
+    @Override
+    public void deleteItemToUserBlacklist(String itemName, String userID) {
+        UserBlacklist userBlacklist = new UserBlacklist(userID);
+        Item i = new Item(itemName);
+        userBlacklist.addItem(i);
+        userBlacklist.removeItem(i);
     }
 }
