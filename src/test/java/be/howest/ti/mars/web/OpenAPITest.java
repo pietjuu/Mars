b3407b4bb438527b3c817b7dd8d5426c46f7755f
glenn.callens <glenn.callens@student.howest.be>
2022-11-17 13:32:53 +0100

    :white_check_mark: add test getShippertBlackList

--- src/test/java/be/howest/ti/mars/web/OpenAPITest.java
+++ src/test/java/be/howest/ti/mars/web/OpenAPITest.java
@@ -124,7 +124,18 @@ class OpenAPITest {
     }
 
     @Test
-    void createErrors(final VertxTestContext testContext){
+    void getShippertBlackList(final VertxTestContext testContext){
+        webClient.get(PORT, HOST, "/api/blacklist").send()
+                .onFailure(testContext::failNow)
+                .onSuccess(response -> testContext.verify(() -> {
+                    assertEquals(200, response.statusCode());
+                    assertEquals("Gun", response.bodyAsJsonObject().getJsonArray("items").getString(0));
+                    testContext.completeNow();
+                }));
+    }
+
+    @Test
+    void createUserErrors(final VertxTestContext testContext){
         webClient.post(PORT, HOST, "/api/users").sendJsonObject(new JsonObject().put("a", 1))
                 .onFailure(testContext::failNow)
                 .onSuccess(response -> testContext.verify(() -> {
