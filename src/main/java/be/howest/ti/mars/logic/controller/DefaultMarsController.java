b45b3755401f98589b8901fa5196e6f533a2bef5
glenn.callens <glenn.callens@student.howest.be>
2022-11-17 11:48:39 +0100

    :bug: bug fix

--- src/main/java/be/howest/ti/mars/logic/controller/DefaultMarsController.java
+++ src/main/java/be/howest/ti/mars/logic/controller/DefaultMarsController.java
@@ -12,7 +12,7 @@ import java.util.*;
 
 /**
  * DefaultMarsController is the default implementation for the MarsController interface.
- * The controller shouldn't even know that it is used in an API context..
+ * The controller shouldn't even know that it is used in an API context.
  * This class and all other classes in the logic-package (or future sub-packages)
  * should use 100% plain old Java Objects (POJOs). The use of Json, JsonObject or
  * Strings that contain encoded/json data should be avoided here.
@@ -84,7 +84,7 @@ public class DefaultMarsController implements MarsController {
 
     @Override
     public List<String> getUserBlacklist(String userID) {
-       if (repository.isUserBlackListExist(userID)){
+       if (!repository.isUserBlackListExist(userID)){
            throw new NoSuchElementException(USERID_BLACKLIST_DOESNT_EXIST);
        }
 
