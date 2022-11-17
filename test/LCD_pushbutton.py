9c7152b3a1d43ca4cc1039f0cd70e3c3f1e9ffbc
Pieter Verheye <pieter.verheye@student.howest.be>
2022-11-17 16:32:36 +0100

    change if structure

--- test/LCD_pushbutton.py
+++ test/LCD_pushbutton.py
@@ -91,7 +91,7 @@ def cleanAndExit():
 
 while True:
     try:
-        if door_closed():
+        if door_closed() & start_1() == False:
             write_ready()
         elif door_open():
             write_not_ready()
