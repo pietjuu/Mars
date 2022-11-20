a4b33f3c1d63a70c77666efe13f7f3e5444b09c4
Thibo Verbeerst <thibo.verbeerst@student.howest.be>
2022-11-20 18:15:39 +0100

    statics route had wrong view, added StaticsView

--- src/views/StatisticsView.vue
+++ src/views/StatisticsView.vue
@@ -1,13 +1,13 @@
 <template>
-$END$
+
 </template>
 
 <script>
 export default {
-name: "StatisticsView"
-}
+  name: "StatisticsView"
+};
 </script>
 
-<style scoped>
+<style scoped lang="scss">
 
 </style>
