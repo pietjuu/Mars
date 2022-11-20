32acbf9858697da77d2880fe8ba6e845d9a854af
Thibo Verbeerst <thibo.verbeerst@student.howest.be>
2022-11-20 20:27:25 +0100

    added 100% value to size mixin

--- src/components/Map/DestinationMap.vue
+++ src/components/Map/DestinationMap.vue
@@ -53,12 +53,11 @@ export default {
     let map = this.createMap("destination-map");
 
   }
-}
+};
 </script>
 
 <style scoped lang="scss">
 #destination-map {
-  width: 100%;
-  height: 100%;
+  @include size(100%);
 }
 </style>
