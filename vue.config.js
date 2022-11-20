aa28b108f485eef05470ba3db3b0800114b5704b
Thibo Verbeerst <thibo.verbeerst@student.howest.be>
2022-11-20 19:10:21 +0100

    debugged mixins import

--- vue.config.js
+++ vue.config.js
@@ -10,7 +10,7 @@ module.exports = defineConfig({
           @use "@/assets/scss/colors.scss";
           @use "@/assets/scss/constants.scss";
           @use "@/assets/scss/fonts.scss";
-          @use "@/assets/scss/mixins.scss";
+          @use "@/assets/scss/mixins.scss" as *;
         `
       }
     }
