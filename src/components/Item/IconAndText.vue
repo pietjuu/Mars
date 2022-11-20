91db9cc24ec3ce935734ff368d7b35e3dd9ddc00
Thibo Verbeerst <thibo.verbeerst@student.howest.be>
2022-11-20 22:23:25 +0100

    created separate component for icon and text in nav item

---
+++ src/components/Item/IconAndText.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="item flex-center-vertical">
+    <div class="item-icon-wrapper">
+      <Icon :icon="icon"/>
+    </div>
+    <div class="item-content-wrapper flex-center-vertical">
+      <p>{{ title }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+import Icon from "@/components/Icon/Icon";
+
+export default {
+  name: "IconAndText",
+  props: {
+    title: String,
+    icon: String
+  },
+  components: {
+    Icon
+  }
+};
+</script>
+
+<style scoped lang="scss">
+
+.item {
+  color: var(--color-text);
+  min-height: 3rem;
+}
+
+.item-content-wrapper {
+  flex-wrap: wrap;
+  margin-left: 0.625rem;
+  padding: 0.75rem 0;
+  flex: 1 1;
+}
+
+.item-content-wrapper p {
+  line-height: 1.2;
+}
+
+.item-icon-wrapper {
+  margin: 16px 0;
+  align-self: flex-start;
+  min-width: 1.5rem;
+  display: inline-flex;
+}
+
+</style>
--- src/components/Navigation/NavigationItem.vue
+++ src/components/Navigation/NavigationItem.vue
@@ -1,16 +1,11 @@
 <template>
-  <router-link :to="route" class="nav-item flex-center-vertical">
-    <div class="nav-icon-wrapper">
-      <Icon :icon="icon"/>
-    </div>
-    <div class="nav-content-wrapper flex-center-vertical">
-      <p>{{ title }}</p>
-    </div>
+  <router-link :to="route" class="no-text-decoration nav-item">
+    <IconAndText :title="title" :icon="icon"/>
   </router-link>
 </template>
 
 <script>
-import Icon from "@/components/Icon/Icon";
+import IconAndText from "@/components/Item/IconAndText";
 
 export default {
   name: "NavigationItem",
@@ -20,7 +15,7 @@ export default {
     icon: String
   },
   components: {
-    Icon
+    IconAndText
   }
 };
 </script>
@@ -28,28 +23,7 @@ export default {
 <style scoped lang="scss">
 
 .nav-item {
-  color: var(--color-text);
-  min-height: 3rem;
   padding: 0 1rem;
-  text-decoration: none;
-}
-
-.nav-content-wrapper {
-  flex-wrap: wrap;
-  margin-left: 0.625rem;
-  padding: 0.75rem 0;
-  flex: 1 1;
-}
-
-.nav-content-wrapper p {
-  line-height: 1.2;
-}
-
-.nav-icon-wrapper {
-  margin: 16px 0;
-  align-self: flex-start;
-  min-width: 1.5rem;
-  display: inline-flex;
 }
 
 </style>
