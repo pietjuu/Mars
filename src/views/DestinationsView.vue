a7d93fcf08e925b477121e325e46b49120cd61ec
Thibo Verbeerst <thibo.verbeerst@student.howest.be>
2022-11-21 08:20:36 +0100

    list of location details with dummy data

--- src/views/DestinationsView.vue
+++ src/views/DestinationsView.vue
@@ -15,6 +15,52 @@
       </div>
       <div class="location-details box">
         <h2>Location Details</h2>
+        <ul>
+          <li>
+            <div class="icon-text-wrapper">
+              <Icon :icon="`badge`"/>
+              <p class="flex-center-vertical">ID: </p>
+            </div>
+            <span class="location-id location-value flex-center-vertical">T-1</span>
+          </li>
+          <li>
+            <div class="icon-text-wrapper">
+              <Icon :icon="`description`"/>
+              <p class="flex-center-vertical">Name: </p>
+            </div>
+            <span class="location-name location-value flex-center-vertical">Farm 200 D2</span>
+          </li>
+          <li>
+            <div class="icon-text-wrapper">
+              <Icon :icon="`category`"/>
+              <p class="flex-center-vertical">Type: </p>
+            </div>
+            <span class="location-type location-value flex-center-vertical">Garbage Point</span>
+          </li>
+          <li>
+            <div class="icon-text-wrapper">
+              <Icon :icon="`square_foot`"/>
+              <p class="flex-center-vertical">Size: </p>
+            </div>
+            <span class="location-size location-value flex-center-vertical">
+              <span class="location-size-length">40cm</span>x<span class="location-size-width">50cm</span>x<span class="location-size-depth">50cm</span>
+            </span>
+          </li>
+          <li>
+            <div class="icon-text-wrapper">
+              <Icon :icon="`my_location`"/>
+              <p class="flex-center-vertical">Longitude: </p>
+            </div>
+            <span class="location-longitude location-value flex-center-vertical">090DFJDD</span>
+          </li>
+          <li>
+            <div class="icon-text-wrapper">
+              <Icon :icon="`my_location`"/>
+              <p class="flex-center-vertical">Latitude: </p>
+            </div>
+            <span class="location-latitude location-value flex-center-vertical">090DFJDD</span>
+          </li>
+        </ul>
       </div>
     </div>
   </main>
@@ -24,13 +70,15 @@
 import HeaderContent from "@/components/Header/HeaderContent";
 import DestinationMap from "@/components/Map/DestinationMap";
 import IconAndText from "@/components/Item/IconAndText";
+import Icon from "@/components/Icon/Icon";
 
 export default {
   name: "DestinationsView",
   components: {
     IconAndText,
     HeaderContent,
-    DestinationMap
+    DestinationMap,
+    Icon
   }
 };
 </script>
@@ -66,6 +114,33 @@ main {
 
 .location-details {
   flex: 1 1 70%;
+
+  ul {
+    display: flex;
+    flex-direction: column;
+    gap: 0.5rem;
+  }
+
+  li {
+    display: flex;
+    gap: 0.25rem;
+  }
+
+}
+
+.location-value {
+  font-weight: bold;
+}
+
+.icon-text-wrapper {
+  display: flex;
+  justify-content: start;
+  gap: 0.5rem;
+}
+
+.location-size {
+  display: flex;
+  gap: 0.25rem;
 }
 
 </style>
