ec87d7a39a9dbd841c07edf865760c2d7838c630
Thibo Verbeerst <thibo.verbeerst@student.howest.be>
2022-11-21 11:02:06 +0100

    added vuex module to fetch transporters

--- src/store/index.js
+++ src/store/index.js
@@ -4,14 +4,16 @@ import { createStore } from "vuex";
 import user from "@/store/modules/user";
 import notification from "@/store/modules/notification";
 import sendItem from "@/store/modules/send-item";
+import transporters from "@/store/modules/transporters";
 
 
 const store = createStore({
     modules: {
         user,
         notification,
-        sendItem
+        sendItem,
+        transporters
     }
-})
+});
 
 export default store;
---
+++ src/store/modules/transporters.js
@@ -0,0 +1,55 @@
+import { get } from "@/assets/js/data-connector/api-communication-abstractor";
+
+function requestStarted(commit) {
+    commit('setTransportersRequest', {
+        error: false,
+        message: "Loading transporters..."
+    });
+}
+
+function requestSuccess(commit, transporters) {
+    commit('setTransporters', transporters);
+    commit('setTransportersRequest', {
+        error: false,
+        message: "Transporters received"
+    });
+}
+
+function requestFailed(commit, error) {
+    commit('setTransportersRequest', {
+        error: true,
+        message: error.statusText
+    });
+}
+
+const state = {
+    transporters: [],
+    request: {
+        error: false,
+        message: ""
+    }
+};
+
+const getters = {
+    transporters: (state) => state.transporters,
+    transportersRequest: (state) => state.request
+};
+
+const actions = {
+    fetchTransporters({ commit }) {
+        requestStarted(commit);
+        get(`transporters`,(transporters) => requestSuccess(commit, transporters),(error) => requestFailed(commit, error));
+    }
+};
+
+const mutations = {
+    setTransporters: (state, transporters) => (state.transporters = transporters),
+    setTransportersRequest: (state, request) => (state.request = request)
+};
+
+export default {
+    state,
+    getters,
+    actions,
+    mutations
+};
