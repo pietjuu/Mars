98a4b81943a248112d034330a3f34452ee0e40a5
glenn.callens <glenn.callens@student.howest.be>
2022-11-24 22:09:25 +0100

    :white_check_mark: add deleteTransporter

--- src/test/java/be/howest/ti/mars/web/OpenAPITest.java
+++ src/test/java/be/howest/ti/mars/web/OpenAPITest.java
@@ -211,6 +211,16 @@ class OpenAPITest {
     }
 
     @Test
+    void deleteTransporter(final VertxTestContext testContext){
+        webClient.delete(PORT, HOST, "/api/transporters/kitchen").bearerTokenAuthentication("te-1").send()
+                .onFailure(testContext::failNow)
+                .onSuccess(response -> testContext.verify(() -> {
+                    assertEquals(202, response.statusCode());
+                    testContext.completeNow();
+                }));
+    }
+
+    @Test
     void createUserErrors(final VertxTestContext testContext){
         webClient.post(PORT, HOST, "/api/users").sendJsonObject(new JsonObject().put("a", 1))
                 .onFailure(testContext::failNow)
