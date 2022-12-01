6182e7cdf83a0cd804ec126af60a3d46710bff00
glenn.callens <glenn.callens@student.howest.be>
2022-12-01 21:48:14 +0100

    :technologist: improve TransporterAPI and split it to TransporterController

---
+++ src/main/java/be/howest/ti/mars/logic/controller/TransporterController.java
@@ -0,0 +1,4 @@
+package be.howest.ti.mars.logic.controller;
+
+public class TransporterController {
+}
--- src/main/java/be/howest/ti/mars/web/external/TransporterAPI.java
+++ src/main/java/be/howest/ti/mars/web/external/TransporterAPI.java
@@ -1,19 +1,14 @@
 package be.howest.ti.mars.web.external;
 
-import be.howest.ti.mars.logic.domain.location.Coordinates;
 import be.howest.ti.mars.logic.domain.transporter.Transporter;
 import be.howest.ti.mars.web.exceptions.TransporterAPIException;
-import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
-import java.net.InetAddress;
 import java.net.URL;
-import java.util.HashMap;
-import java.util.Map;
 
 public class TransporterAPI {
 
@@ -29,39 +24,27 @@ public class TransporterAPI {
         this.ipAddress = ip;
     }
 
-    public boolean isTransporterReady(){
-        JsonObject result = sendGET("/api/status");
-
-        assert result != null;
-        return result.getBoolean("ready");
+    public JsonObject getLocation(){
+        return callAPI("/api/location", "GET");
     }
 
-    public Coordinates getTransporterCoordinates(){
-        JsonObject result = sendGET("/api/location");
-
-        assert result != null;
-        return new Coordinates(result.getFloat("longitude"), result.getFloat("latitude"));
+    public JsonObject getStatus(){
+        return callAPI("/api/location", "GET");
     }
 
-    public Map<String, Integer> getScanSummary(){
-        JsonObject result = sendGET("/api/scan");
-
-        assert result != null;
-        Map<String, Integer> re = new HashMap<>();
-
-        for (int i = 0; i < result.getJsonArray("summary").size(); i++){
-            JsonObject jsonObject = result.getJsonArray("summary").getJsonObject(i);
-            re.put(jsonObject.getString("molecule"), jsonObject.getInteger("count"));
-        }
-
-        return re;
+    public JsonObject getScan(){
+        return callAPI("/api/scan", "GET");
     }
 
-    private JsonObject sendGET(String params){
+    public JsonObject addSend(){
+        return callAPI("/api/send", "POST");
+    }
+
+    private JsonObject callAPI(String params, String method){
         try {
             URL obj = new URL(ipAddress + params);
             HttpURLConnection con = (HttpURLConnection) obj.openConnection();
-            con.setRequestMethod("GET");
+            con.setRequestMethod(method);
             con.setRequestProperty("User-Agent", "Mozilla/5.0");
             int responseCode = con.getResponseCode();
             if (responseCode == HttpURLConnection.HTTP_OK) {
@@ -75,11 +58,20 @@ public class TransporterAPI {
                 in.close();
 
                 return new JsonObject(response.toString());
+            } else {
+                BufferedReader in = new BufferedReader(new InputStreamReader(con.getErrorStream()));
+                String inputLine;
+                StringBuilder response = new StringBuilder();
+
+                while ((inputLine = in.readLine()) != null) {
+                    response.append(inputLine);
+                }
+                in.close();
+
+                throw new TransporterAPIException(responseCode, new JsonObject(response.toString()).getString("description"));
             }
         } catch (IOException e){
-            System.out.println(e);
-            throw new TransporterAPIException("Transporter is unavailable!");
+            throw new TransporterAPIException(404, "Transporter is unavailable!");
         }
-        return null;
     }
 }
