50f67a3492d5828aef61c82f044468d9bbfd8530
glenn.callens <glenn.callens@student.howest.be>
2022-12-01 22:00:53 +0100

    :construction: add getTransporterLocation

--- src/main/java/be/howest/ti/mars/logic/controller/TransporterController.java
+++ src/main/java/be/howest/ti/mars/logic/controller/TransporterController.java
@@ -1,4 +1,25 @@
 package be.howest.ti.mars.logic.controller;
 
+import be.howest.ti.mars.logic.domain.location.Coordinates;
+import be.howest.ti.mars.logic.domain.transporter.Transporter;
+import be.howest.ti.mars.logic.exceptions.TransporterException;
+import be.howest.ti.mars.web.external.TransporterAPI;
+import io.vertx.core.json.JsonObject;
+
 public class TransporterController {
+
+    public Coordinates getTransporterLocation(Transporter transporter){
+        TransporterAPI transporterAPI = new TransporterAPI(transporter);
+        JsonObject jsonObject = transporterAPI.getLocation();
+
+        Coordinates coordinatesAPI = new Coordinates(jsonObject.getFloat("latitude"), jsonObject.getFloat("longitude"));
+        Coordinates coordinatesDB = transporter.getBuilding().getCoordinates();
+
+        if (!coordinatesDB.equals(coordinatesAPI)){
+            throw new TransporterException("Transporter location not the same!");
+        }
+
+        return coordinatesDB;
+    }
+
 }
