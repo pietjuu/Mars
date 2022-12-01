ee248a0fee34cadac96d5f31a4bdf1886953cedc
glenn.callens <glenn.callens@student.howest.be>
2022-12-01 23:42:28 +0100

    :construction: add getCost()

--- src/main/java/be/howest/ti/mars/logic/domain/molecule/MoleculesSummary.java
+++ src/main/java/be/howest/ti/mars/logic/domain/molecule/MoleculesSummary.java
@@ -1,16 +1,21 @@
 package be.howest.ti.mars.logic.domain.molecule;
 
+import be.howest.ti.mars.logic.utils.CostCalculator;
+
 import java.util.Map;
 import java.util.Set;
 
 public class MoleculesSummary {
 
-    private final Map<String , Integer> summary;
+    private final Map<String, Integer> summary;
     private final Set<Molecule> molecules;
 
-    public MoleculesSummary(Map<String , Integer> summary, Set<Molecule> molecules){
+    private final double cost;
+
+    public MoleculesSummary(Map<String, Integer> summary, Set<Molecule> molecules) {
         this.summary = summary;
         this.molecules = molecules;
+        this.cost = CostCalculator.calculateTotalCost(this.summary);
     }
 
     public Map<String, Integer> getSummary() {
@@ -19,5 +24,9 @@ public class MoleculesSummary {
 
     public Set<Molecule> getMolecules() {
         return molecules;
+    }
+
+    public double getCost() {
+        return cost;
     }
 }
