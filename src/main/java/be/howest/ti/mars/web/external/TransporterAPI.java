b8d68bac9b0467456efe89c5b3c72f1b6b987f91
glenn.callens <glenn.callens@student.howest.be>
2022-12-01 20:30:50 +0100

    :construction: add sendGET

--- src/main/java/be/howest/ti/mars/web/external/TransporterAPI.java
+++ src/main/java/be/howest/ti/mars/web/external/TransporterAPI.java
@@ -2,8 +2,12 @@ package be.howest.ti.mars.web.external;
 
 import be.howest.ti.mars.logic.domain.transporter.Transporter;
 import be.howest.ti.mars.web.exceptions.TransporterAPIException;
+import io.vertx.core.json.JsonObject;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
 import java.net.InetAddress;
 import java.net.URL;
 
@@ -26,6 +30,30 @@ public class TransporterAPI {
             }
 
             this.ipAddress = result.toString();
+        } catch (IOException e){
+            throw new TransporterAPIException("Transporter is unavailable!");
+        }
+    }
+
+    private JsonObject sendGET(String params) throws IOException {
+        try {
+            URL obj = new URL(ipAddress + params);
+            HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+            con.setRequestMethod("GET");
+            con.setRequestProperty("User-Agent", "Mozilla/5.0");
+            int responseCode = con.getResponseCode();
+            if (responseCode == HttpURLConnection.HTTP_OK) {
+                BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+                String inputLine;
+                StringBuilder response = new StringBuilder();
+
+                while ((inputLine = in.readLine()) != null) {
+                    response.append(inputLine);
+                }
+                in.close();
+
+                return new JsonObject(response.toString());
+            }
         } catch (IOException e){
             throw new TransporterAPIException("Transporter is unavailable!");
         }
