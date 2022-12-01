d906dbf231596183374c0f15c3086ec51bdb1f4c
glenn.callens <glenn.callens@student.howest.be>
2022-12-01 21:50:52 +0100

    :goal_net: add TransporterException

---
+++ src/main/java/be/howest/ti/mars/logic/exceptions/TransporterException.java
@@ -0,0 +1,9 @@
+package be.howest.ti.mars.logic.exceptions;
+
+public class TransporterException extends RuntimeException{
+
+    public TransporterException(String message){
+        super(message);
+    }
+
+}
