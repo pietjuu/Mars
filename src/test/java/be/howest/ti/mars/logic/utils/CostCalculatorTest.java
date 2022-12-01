fa13bdcabcc2898ca995ebfaf03ce5e0d0c41b58
glenn.callens <glenn.callens@student.howest.be>
2022-12-02 00:44:03 +0100

    :white_check_mark: add test CostCalculator

--- src/test/java/be/howest/ti/mars/logic/utils/CostCalculatorTest.java
+++ src/test/java/be/howest/ti/mars/logic/utils/CostCalculatorTest.java
@@ -1,12 +1,5 @@
 package be.howest.ti.mars.logic.utils;
-
-import be.howest.ti.mars.logic.domain.molecule.Molecule;
 import org.junit.jupiter.api.Test;
-
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.*;
 
