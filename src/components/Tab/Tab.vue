a80455711a92577553f14ce376e100eec4c3938e
Thibo Verbeerst <thibo.verbeerst@student.howest.be>
2022-12-08 14:33:36 +0100

    created tab component for tab bar

---
+++ src/components/Tab/Tab.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="blacklist-tab">
+    <p>{{ text }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Tab",
+  props: {
+    text: String
+  }
+};
+
+</script>
+
+<style scoped lang="scss">
+
+p {
+  font-size: var(--large-text-size);
+}
+
+</style>
