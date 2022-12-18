e312f4554997fa87fcf5c8119cb6c9af9aa5ea48
Thibo Verbeerst <thibo.verbeerst@student.howest.be>
2022-12-18 21:06:52 +0100

    fetch blacklist items

--- src/store/modules/blacklist.js
+++ src/store/modules/blacklist.js
@@ -1,10 +1,41 @@
-const state = {};
+import { TOKEN } from "@/main";
+import { get } from "@/assets/js/data-connector/api-communication-abstractor";
 
-const getters = {};
+const state = {
+    userBlacklist: undefined,
+    shippertBlacklist: undefined
+};
 
-const actions = {};
+const getters = {
+    userBlacklist: (state) => state.userBlacklist,
+    shippertBlacklist: (state) => state.shippertBlacklist
+};
 
-const mutations = {};
+const actions = {
+    async fetchUserBlacklist({ commit }) {
+        await get(`users/${TOKEN}/blacklist`,(blacklist) => {
+            commit('setUserBlacklist', blacklist);
+        });
+    },
+    async fetchShippertBlacklist({ commit }) {
+        await get(`blacklist`,(blacklist) => {
+            commit('setShippertBlacklist', blacklist);
+        });
+    },
+    async fetchAllBlacklists({ commit }) {
+        await get(`users/${TOKEN}/blacklist`,(blacklist) => {
+            commit('setUserBlacklist', blacklist);
+        });
+        await get(`blacklist`,(blacklist) => {
+            commit('setShippertBlacklist', blacklist);
+        });
+    }
+};
+
+const mutations = {
+    setUserBlacklist: (state, blacklist) => (state.userBlacklist = blacklist),
+    setShippertBlacklist: (state, blacklist) => (state.shippertBlacklist = blacklist)
+};
 
 export default {
     state,
