fc409035329e7c92a003e2629649bae8d787d0be
Thibo Verbeerst <thibo.verbeerst@student.howest.be>
2022-12-18 23:31:09 +0100

    setup grid layout for dashboard

--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -1,15 +1,55 @@
 <template>
-  <div>
-    content
-  </div>
+  <HeaderContent :title="`Dashboard`"/>
+  <main class="main-content">
+    <div class="box">
+      test
+    </div>
+    <div class="box">
+      test
+    </div>
+    <div class="box">
+      test
+    </div>
+    <div class="box">
+      test
+    </div>
+    <div class="box">
+      test
+    </div>
+    <div class="box">
+      test
+    </div>
+    <div class="box notification">
+      test
+    </div>
+  </main>
 </template>
 
 <script>
+import HeaderContent from "@/components/Header/HeaderContent.vue";
+
 export default {
-  name: "DashboardView"
+  name: "DashboardView",
+  components: {HeaderContent}
 }
 </script>
 
 <style scoped lang="scss">
+
+.main-content {
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-template-rows: repeat(3, 10rem);
+  grid-gap: 1.25rem;
+  grid-template-areas:
+    ". . . ."
+    ". notif notif notif"
+    ". notif notif notif";
+
+}
+
+.notification {
+  grid-area: notif;
+}
 
 </style>
