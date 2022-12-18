f371906566318de794754636423c345d0141eae5
Thibo Verbeerst <thibo.verbeerst@student.howest.be>
2022-12-18 21:11:40 +0100

    list blacklist items of both user and shippert

--- src/views/BlacklistView.vue
+++ src/views/BlacklistView.vue
@@ -7,13 +7,19 @@
         <input type="text" id="new-blacklist-item" name="new-blacklist-item" required autocomplete="off" placeholder="Add new blacklist item"/>
         <IconButton :icon="`add_circle`" :color="`var(--color-primary-soft)`"/>
       </div>
-      <div>
-        <div>ddd</div>
-        <div>fdff</div>
+      <div class="blacklist-items" v-if="this.userBlacklist !== undefined">
+        <div v-for="item in this.userBlacklist.items" :id="item" class="box flex-space-between-row flex-center-vertical">
+          <p>{{ item }}</p>
+          <IconButton :icon="`delete`" :color="`var(--color-primary-soft)`"/>
+        </div>
       </div>
     </div>
     <div class="tab-content" v-if="currentTab === `Shippert Blacklist`">
-      test
+      <div class="blacklist-items" v-if="this.shippertBlacklist !== undefined">
+        <div v-for="item in this.shippertBlacklist.items" :id="item" class="box">
+          <p>{{ item }}</p>
+        </div>
+      </div>
     </div>
   </main>
 </template>
@@ -22,6 +28,7 @@
 import HeaderContent from "@/components/Header/HeaderContent";
 import TabBar from "@/components/Tab/TabBar.vue";
 import IconButton from "@/components/Button/IconButton.vue";
+import {mapActions, mapGetters} from "vuex";
 
 export default {
   name: "BlacklistView",
@@ -37,13 +44,20 @@ export default {
         { name:'Shippert Blacklist', index:1 }
       ],
       currentTab: 'My Blacklist'
-    }
+    };
   },
   methods: {
+    ...mapActions(['fetchAllBlacklists']),
     setCurrentTab(tab) {
       if(!tab.target.closest('button')) { return; }
       this.currentTab = tab.target.dataset.tab;
     }
+  },
+  computed: {
+    ...mapGetters(['userBlacklist', 'shippertBlacklist'])
+  },
+  async created() {
+    await this.fetchAllBlacklists();
   }
 };
 
@@ -55,6 +69,12 @@ export default {
   display: flex;
   flex-direction: column;
   gap: 2rem;
+}
+
+.blacklist-items {
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
 }
 
 </style>
