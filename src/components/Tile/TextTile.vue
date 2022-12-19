18bf169f6fea8770254d3d5e74d9fd52573cbbc3
Thibo Verbeerst <thibo.verbeerst@student.howest.be>
2022-12-19 08:49:53 +0100

    created text tile component

---
+++ src/components/Tile/TextTile.vue
@@ -0,0 +1,29 @@
+<template>
+  <article class="box tile">
+    <header class="flex-space-between-row flex-align-top">
+      <h2>{{ title }}</h2>
+      <Icon :icon="icon"/>
+    </header>
+    <main class="flex-center-vertical">
+      <p class="important">{{ text }}</p>
+    </main>
+  </article>
+</template>
+
+<script>
+import Icon from "@/components/Icon/Icon.vue";
+
+export default {
+  name: "TextTile",
+  components: {Icon},
+  props: {
+    title: String,
+    icon: String,
+    text: String
+  }
+};
+</script>
+
+<style scoped lang="scss">
+
+</style>
