3e287cd1732420a0d644d75e1cab519b434a7ece
Thibo Verbeerst <thibo.verbeerst@student.howest.be>
2022-12-20 16:22:31 +0100

    created vuex component for users

---
+++ src/store/modules/shippert/users/users.js
@@ -0,0 +1,26 @@
+import { get } from "@/assets/js/data-connector/api-communication-abstractor";
+
+const state = {
+    users: undefined
+};
+
+const getters = {
+    users: (state) => state.users
+};
+
+const actions = {
+    async fetchUsers({ commit }) {
+        await get(`users`,users => commit('setUsers', users));
+    }
+};
+
+const mutations = {
+    setUsers: (state, users) => (state.users = users)
+};
+
+export default {
+    state,
+    getters,
+    actions,
+    mutations
+};
