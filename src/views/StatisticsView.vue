610d6e070be18eac04c2d0033217654af521b404
Thibo Verbeerst <thibo.verbeerst@student.howest.be>
2022-12-20 15:37:51 +0100

    added positioning for two charts and resize chart every 500ms and added more dummy items

--- src/views/StatisticsView.vue
+++ src/views/StatisticsView.vue
@@ -7,8 +7,13 @@
       <TextTile class="tile-item" :title="`Total number of received items`" :icon="`archive`" :text="`${this.user.totalReceived}`"/>
       <TextTile class="tile-item" :title="`Total number of sent/received items`" :icon="`all_inbox`" :text="`${this.user.totalSent +  this.user.totalReceived}`"/>
     </div>
-    <div class="box">
-      <ItemsPerDayLineChart :items="items"/>
+    <div class="flex-gap-row charts">
+      <div class="box chart-1">
+        <ItemsPerDayLineChart :items="items"/>
+      </div>
+      <div class="box chart-2">
+        <DestinationsPieChart :items="items"/>
+      </div>
     </div>
   </main>
 </template>
@@ -18,10 +23,12 @@ import HeaderContent from "@/components/Header/HeaderContent.vue";
 import {mapGetters} from "vuex";
 import TextTile from "@/components/Tile/TextTile.vue";
 import ItemsPerDayLineChart from "@/components/Graph/ItemsPerDayLineChart.vue";
+import DestinationsPieChart from "@/components/Graph/DestinationsPieChart.vue";
+import {Chart as ChartJS} from "chart.js";
 
 export default {
   name: "StatisticsView",
-  components: {ItemsPerDayLineChart, TextTile, HeaderContent},
+  components: {DestinationsPieChart, ItemsPerDayLineChart, TextTile, HeaderContent},
   computed: {
     ...mapGetters(['user'])
   },
@@ -37,7 +44,7 @@ export default {
           "receiver": "133",
           "origin": "232",
           "sender": "34",
-          "destination": "332"
+          "destination": "TT-3"
         },
         {
           "id": "1212",
@@ -48,7 +55,7 @@ export default {
           "receiver": "133",
           "origin": "232",
           "sender": "34",
-          "destination": "332"
+          "destination": "TT-2"
         },
         {
           "id": "1212",
@@ -59,7 +66,7 @@ export default {
           "receiver": "133",
           "origin": "232",
           "sender": "34",
-          "destination": "332"
+          "destination": "TT-1"
         },
         {
           "id": "1211",
@@ -70,7 +77,7 @@ export default {
           "receiver": "133",
           "origin": "232",
           "sender": "34",
-          "destination": "332"
+          "destination": "TT-1"
         },
         {
           "id": "1212",
@@ -81,7 +88,7 @@ export default {
           "receiver": "133",
           "origin": "232",
           "sender": "34",
-          "destination": "332"
+          "destination": "TT-2"
         },
         {
           "id": "1212",
@@ -92,10 +99,134 @@ export default {
           "receiver": "133",
           "origin": "232",
           "sender": "34",
-          "destination": "332"
+          "destination": "TT-2"
+        },
+        {
+          "id": "1211",
+          "name": "Alarm Clock",
+          "action": "sent",
+          "timeSent": "2053-12-24 09:47",
+          "timeReceived": "2053-12-24 09:48",
+          "receiver": "133",
+          "origin": "232",
+          "sender": "34",
+          "destination": "TT-3"
+        },
+        {
+          "id": "1212",
+          "name": "Alarm Clock",
+          "action": "sent",
+          "timeSent": "2053-12-24 09:47",
+          "timeReceived": "2053-12-24 09:48",
+          "receiver": "133",
+          "origin": "232",
+          "sender": "34",
+          "destination": "TT-5"
+        },
+        {
+          "id": "1212",
+          "name": "Alarm Clock",
+          "action": "sent",
+          "timeSent": "2053-12-25 09:47",
+          "timeReceived": "2053-12-24 09:48",
+          "receiver": "133",
+          "origin": "232",
+          "sender": "34",
+          "destination": "TT-4"
+        },
+        {
+          "id": "1211",
+          "name": "Alarm Clock",
+          "action": "received",
+          "timeSent": "2053-12-24 09:47",
+          "timeReceived": "2053-12-24 09:48",
+          "receiver": "133",
+          "origin": "232",
+          "sender": "34",
+          "destination": "TT-5"
+        },
+        {
+          "id": "1212",
+          "name": "Alarm Clock",
+          "action": "received",
+          "timeSent": "2053-12-25 09:47",
+          "timeReceived": "2053-12-25 09:48",
+          "receiver": "133",
+          "origin": "232",
+          "sender": "34",
+          "destination": "TT-5"
+        },
+        {
+          "id": "1212",
+          "name": "Alarm Clock",
+          "action": "received",
+          "timeSent": "2053-12-27 09:47",
+          "timeReceived": "2053-12-29 09:48",
+          "receiver": "133",
+          "origin": "232",
+          "sender": "34",
+          "destination": "TT-6"
+        },
+        {
+          "id": "1212",
+          "name": "Alarm Clock",
+          "action": "sent",
+          "timeSent": "2053-12-25 09:47",
+          "timeReceived": "2053-12-24 09:48",
+          "receiver": "133",
+          "origin": "232",
+          "sender": "34",
+          "destination": "TT-4"
+        },
+        {
+          "id": "1211",
+          "name": "Alarm Clock",
+          "action": "sent",
+          "timeSent": "2053-12-24 09:47",
+          "timeReceived": "2053-12-24 09:48",
+          "receiver": "133",
+          "origin": "232",
+          "sender": "34",
+          "destination": "TT-5"
+        },
+        {
+          "id": "1212",
+          "name": "Alarm Clock",
+          "action": "sent",
+          "timeSent": "2053-12-25 09:47",
+          "timeReceived": "2053-12-25 09:48",
+          "receiver": "133",
+          "origin": "232",
+          "sender": "34",
+          "destination": "TT-5"
+        },
+        {
+          "id": "1212",
+          "name": "Alarm Clock",
+          "action": "sent",
+          "timeSent": "2053-12-27 09:47",
+          "timeReceived": "2053-12-29 09:48",
+          "receiver": "133",
+          "origin": "232",
+          "sender": "34",
+          "destination": "TT-6"
         }
       ]
     };
+  },
+  methods: {
+    resizeCharts() {
+      for (const id in ChartJS.instances) {
+        ChartJS.instances[id].resize();
+      }
+      this.timeout = setTimeout(this.resizeCharts, 500);
+    }
+  },
+  mounted() {
+    this.resizeCharts();
+  },
+  unmounted() {
+    clearTimeout(this.timeout);
   }
 };
 </script>
@@ -104,6 +235,20 @@ export default {
 
 .tile-item {
   flex: 1 1 100%;
+}
+
+.chart-1 {
+  flex: 1 1 65%;
+}
+
+.chart-2 {
+  flex: 1 1 30%;
+}
+
+@media (max-width: 2048px) {
+  .charts {
+    flex-direction: column;
+  }
 }
 
 </style>
