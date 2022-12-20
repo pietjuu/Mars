2b6491cf09168fd3f7a2c0b20e1e984ecf3c35fc
glenn.callens <glenn.callens@student.howest.be>
2022-12-20 10:57:41 +0100

    :white_check_mark: add  connectLink tests

--- src/test/java/be/howest/ti/mars/logic/domain/link/LinkTest.java
+++ src/test/java/be/howest/ti/mars/logic/domain/link/LinkTest.java
@@ -1,11 +1,15 @@
 package be.howest.ti.mars.logic.domain.link;
 
+import be.howest.ti.mars.logic.data.InMemoryRepository;
 import be.howest.ti.mars.logic.domain.items.Item;
 import be.howest.ti.mars.logic.domain.location.Building;
 import be.howest.ti.mars.logic.domain.location.Coordinates;
 import be.howest.ti.mars.logic.domain.location.TypeOfLocation;
 import be.howest.ti.mars.logic.domain.transporter.Size;
 import be.howest.ti.mars.logic.domain.transporter.Transporter;
+import be.howest.ti.mars.logic.domain.users.User;
+import be.howest.ti.mars.logic.utils.MockInformation;
+import org.jcodings.exception.TranscoderException;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -21,9 +25,69 @@ class LinkTest {
 
     @Test
     void constructorImport(){
-        Link l1 = new Link("TL-1", new Transporter("TT-1", "TT-1", new Size(10f, 10f, 10f), new Building(TypeOfLocation.RESIDENCE, new Coordinates(1f, 1f)), "https://transporter1.thibo.cloud/"), new Transporter("TT-2", "TT-2", new Size(10f, 10f, 10f), new Building(TypeOfLocation.RESIDENCE, new Coordinates(1f, 1f)), "https://transporter1.thibo.cloud/"), LinkStatus.SENT, new Item("Apple"));
+        InMemoryRepository inMemoryRepository = new InMemoryRepository();
+        Link l1 = new Link("TL-T", inMemoryRepository.getUser("T-1"), inMemoryRepository.getUser("T-2"), inMemoryRepository.getTransporter("TT-4"), inMemoryRepository.getTransporter("TT-5"), LinkStatus.SENT, new Item("Apple", MockInformation.getMoleculesSummary()));
 
         assertEquals(LinkStatus.SENT, l1.getLinkStatus());
     }
 
+    @Test
+    void connectLink(){
+        InMemoryRepository inMemoryRepository = new InMemoryRepository();
+        User senderUser = inMemoryRepository.getUser("T-1");
+        User receiverUser = inMemoryRepository.getUser("T-2");
+
+        Transporter sender = inMemoryRepository.getTransporter("TT-4");
+        Transporter receiver = inMemoryRepository.getTransporter("TT-5");
+
+        Link link = new Link(sender);
+        link.connectLink(senderUser, sender, receiverUser, receiver, "Apple", senderUser.getPricePlan().getMaxItems());
+
+        assertEquals(LinkStatus.LINKED, link.getLinkStatus());
+    }
+
+    @Test
+    void connectLinkMaxItems(){
+        InMemoryRepository inMemoryRepository = new InMemoryRepository();
+        User senderUser = inMemoryRepository.getUser("T-1");
+        User receiverUser = inMemoryRepository.getUser("T-2");
+
+        Transporter sender = inMemoryRepository.getTransporter("TT-4");
+        Transporter receiver = inMemoryRepository.getTransporter("TT-5");
+
+        Link link = new Link(sender);
+        assertThrows(TranscoderException.class, () -> link.connectLink(senderUser, sender, receiverUser, receiver, "Apple", 1));
+
+    }
+
+    @Test
+    void connectLinkFits(){
+        InMemoryRepository inMemoryRepository = new InMemoryRepository();
+        User senderUser = inMemoryRepository.getUser("T-1");
+        User receiverUser = inMemoryRepository.getUser("T-2");
+
+        Transporter sender = inMemoryRepository.getTransporter("TT-4");
+        sender.setSize(new Size(0.1f, 0.1f, 0.1f));
+
+        Transporter receiver = inMemoryRepository.getTransporter("TT-5");
+
+        Link link = new Link(sender);
+        assertThrows(TranscoderException.class, () -> link.connectLink(senderUser, sender, receiverUser, receiver, "Apple", 1));
+
+    }
+
+    @Test
+    void connectLinkFits2(){
+        InMemoryRepository inMemoryRepository = new InMemoryRepository();
+        User senderUser = inMemoryRepository.getUser("T-1");
+        User receiverUser = inMemoryRepository.getUser("T-2");
+
+        Transporter sender = inMemoryRepository.getTransporter("TT-4");
+        Transporter receiver = inMemoryRepository.getTransporter("TT-5");
+        receiver.setSize(new Size(0.1f, 0.1f, 0.1f));
+
+        Link link = new Link(sender);
+        assertThrows(TranscoderException.class, () -> link.connectLink(senderUser, sender, receiverUser, receiver, "Apple", 1));
+
+    }
 }
