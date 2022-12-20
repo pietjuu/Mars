5ff67ba3f1538cc7b7c51aa806284b89b03770c2
Thibo Verbeerst <thibo.verbeerst@student.howest.be>
2022-12-21 00:17:46 +0100

    created dropdown component

---
+++ src/components/Form/Dropdown.vue
@@ -0,0 +1,30 @@
+<template>
+  <select :name="name" class="box">
+    <option v-for="value in values" :value="value" :selected="value === selected" @click="selected = value">{{ value }}</option>
+  </select>
+</template>
+
+<script>
+export default {
+  name: "Dropdown",
+  props: {
+    name: String,
+    values: Array
+  },
+  data() {
+    return {
+      selected: this.values[0]
+    };
+  },
+  methods: {
+    getSelected() {
+      this.$emit('selected', this.selected);
+    }
+  }
+};
+</script>
+
+<style scoped lang="scss">
+
+
+</style>
