670301777d46df614015b3782344720a5479e391
glenn.callens <glenn.callens@student.howest.be>
2022-12-20 11:54:04 +0100

    :white_check_mark: add test initConnection

--- src/test/java/be/howest/ti/mars/web/OpenAPITest.java
+++ src/test/java/be/howest/ti/mars/web/OpenAPITest.java
@@ -231,6 +231,16 @@ class OpenAPITest {
     }
 
     @Test
+    void initConnection(final VertxTestContext testContext){
+        webClient.get(PORT, HOST, "/api/transporters/testingTransporter/init").bearerTokenAuthentication("te-1").send()
+                .onFailure(testContext::failNow)
+                .onSuccess(response -> testContext.verify(() -> {
+                    assertEquals(201, response.statusCode());
+                    testContext.completeNow();
+                }));
+    }
+
+    @Test
     void createUserErrors(final VertxTestContext testContext){
         webClient.post(PORT, HOST, "/api/users").sendJsonObject(new JsonObject().put("a", 1))
                 .onFailure(testContext::failNow)
