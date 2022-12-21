01856c6744f3a09e31e4751920afdff20a762f54
glenn.callens <glenn.callens@student.howest.be>
2022-12-21 20:32:09 +0100

    :sparkles: add SystemNotification

---
+++ src/main/java/be/howest/ti/mars/logic/domain/notifications/SystemNotification.java
@@ -0,0 +1,7 @@
+package be.howest.ti.mars.logic.domain.notifications;
+
+public class SystemNotification extends Notification{
+    public SystemNotification(String title, int daysActive, String message) {
+        super(title, daysActive, message);
+    }
+}
