cfe9ec3588b6c573090673ac67fa9ba068203f71
glenn.callens <glenn.callens@student.howest.be>
2022-12-21 22:45:35 +0100

    :alien: add notifications to detailedUser endpoint

--- src/main/java/be/howest/ti/mars/web/bridge/MarsOpenApiBridge.java
+++ src/main/java/be/howest/ti/mars/web/bridge/MarsOpenApiBridge.java
@@ -152,7 +152,7 @@ public class MarsOpenApiBridge {
     private void getUser(RoutingContext routingContext){
         String id = Request.from(routingContext).getUserID();
 
-        Response.sendDetailUser(routingContext, controller.getUser(id), controller.getLinksSent(id), controller.getLinksReceived(id), controller.getLinksSentToday(id));
+        Response.sendDetailUser(routingContext, controller.getUser(id), controller.getLinksSent(id), controller.getLinksReceived(id), controller.getLinksSentToday(id), controller.getNotifications(id));
 
     }
 
