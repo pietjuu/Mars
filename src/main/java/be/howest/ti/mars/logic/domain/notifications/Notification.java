66e6c68d307f7dad6dd9dc98e33e69219bb83082
glenn.callens <glenn.callens@student.howest.be>
2022-12-21 19:07:49 +0100

    :sparkles: add Notification

---
+++ src/main/java/be/howest/ti/mars/logic/domain/notifications/Notification.java
@@ -0,0 +1,43 @@
+package be.howest.ti.mars.logic.domain.notifications;
+
+import java.time.LocalDateTime;
+
+public class Notification {
+
+    private final String title;
+    private final LocalDateTime time;
+    private final LocalDateTime expireTime;
+    private final String message;
+
+    public Notification(String title, int daysActive, String message) {
+        this.title = title;
+        this.time = LocalDateTime.now();
+        this.expireTime = this.time.plusDays(daysActive);
+        this.message = message;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public LocalDateTime getTime() {
+        return time;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public LocalDateTime getExpireTime() {
+        return expireTime;
+    }
+
+    @Override
+    public String toString() {
+        return "Notification: \n" +
+                "title:" + title + "\n" +
+                "time:" + time + "\n" +
+                "message" + message + "\n";
+    }
+}
+
