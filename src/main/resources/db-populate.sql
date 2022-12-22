21935b1676db2f611b6d6d9faa4500263821eaee
glenn.callens <glenn.callens@student.howest.be>
2022-12-22 01:44:42 +0100

    :clown_face: temp mock data building

--- src/main/resources/db-populate.sql
+++ src/main/resources/db-populate.sql
@@ -1,2 +1,3 @@
 INSERT INTO Users (uid, firstname, lastname, pricePlan) VALUES ('T-2', 'Thibo', 'Verbeerst', 'PREMIUM');
 INSERT INTO Users (uid, firstname, lastname, pricePlan) VALUES ('T-1', 'Glenn', 'Callens', 'STANDARD');
+INSERT INTO Buildings (uid, typeOfLocation, longitude, latitude) VALUES ('B-1', 'RESIDENCE', -21.33977, 124.86297);
