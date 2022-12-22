1918edcb88040c98913c52678a7c321769b4c9c9
glenn.callens <glenn.callens@student.howest.be>
2022-12-22 01:39:25 +0100

    :card_file_box: add table buildings

--- src/main/resources/db-create.sql
+++ src/main/resources/db-create.sql
@@ -4,3 +4,10 @@ CREATE TABLE Users(
                       lastname varchar(256) not null ,
                       pricePlan varchar(256) not null
 );
+
+CREATE TABLE Buildings(
+                          uid varchar(256) primary key not null,
+                          typeOfLocation varchar(256) not null ,
+                          longitude float not null ,
+                          latitude float not null
+);
