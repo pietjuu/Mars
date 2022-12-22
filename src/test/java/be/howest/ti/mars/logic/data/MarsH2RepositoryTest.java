54d9668f01b4318ee36b7ffd3aa57023616701f8
glenn.callens <glenn.callens@student.howest.be>
2022-12-22 12:06:40 +0100

    :white_check_mark: add test template

--- src/test/java/be/howest/ti/mars/logic/data/MarsH2RepositoryTest.java
+++ src/test/java/be/howest/ti/mars/logic/data/MarsH2RepositoryTest.java
@@ -1,7 +1,116 @@
 package be.howest.ti.mars.logic.data;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 class MarsH2RepositoryTest {
 
+    @Test
+    void addUser() {
+    }
 
+    @Test
+    void getUsers() {
+    }
 
+    @Test
+    void getUser() {
+    }
+
+    @Test
+    void deleteUser() {
+    }
+
+    @Test
+    void getShippertBlacklist() {
+    }
+
+    @Test
+    void getUserBlacklist() {
+    }
+
+    @Test
+    void createUserBlacklist() {
+    }
+
+    @Test
+    void addItemToUserBlacklist() {
+    }
+
+    @Test
+    void removeItemToUserBlacklist() {
+    }
+
+    @Test
+    void isUserBlackListExist() {
+    }
+
+    @Test
+    void getTransporters() {
+    }
+
+    @Test
+    void addTransporter() {
+    }
+
+    @Test
+    void getTransporter() {
+    }
+
+    @Test
+    void updateTransporter() {
+    }
+
+    @Test
+    void deleteTransporter() {
+    }
+
+    @Test
+    void addBuilding() {
+    }
+
+    @Test
+    void removeBuilding() {
+    }
+
+    @Test
+    void getBuilding() {
+    }
+
+    @Test
+    void getBuildingFromCoordinates() {
+    }
+
+    @Test
+    void getAllLinks() {
+    }
+
+    @Test
+    void getLink() {
+    }
+
+    @Test
+    void addLink() {
+    }
+
+    @Test
+    void deleteLink() {
+    }
+
+    @Test
+    void getShipNotifications() {
+    }
+
+    @Test
+    void getSystemNotifications() {
+    }
+
+    @Test
+    void addShipNotification() {
+    }
+
+    @Test
+    void addSystemNotification() {
+    }
 }
