c84e2aa6973bab1761d70456daea21068e943b7e
glenn.callens <glenn.callens@student.howest.be>
2022-12-22 01:39:09 +0100

    :card_file_box: add table users

--- src/main/resources/db-create.sql
+++ src/main/resources/db-create.sql
@@ -1,5 +1,6 @@
-create table quotes
-(
-    id    int auto_increment,
-    quote varchar(255)
+CREATE TABLE Users(
+                      uid varchar(256) primary key not null,
+                      firstname varchar(256) not null ,
+                      lastname varchar(256) not null ,
+                      pricePlan varchar(256) not null
 );
