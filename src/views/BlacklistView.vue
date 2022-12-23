d9776417c6c8eb430f19e7579a27bd5219a71bae
Thibo Verbeerst <thibo.verbeerst@student.howest.be>
2022-12-23 11:01:30 +0100

    added security measures when creating item

--- src/views/BlacklistView.vue
+++ src/views/BlacklistView.vue
@@ -47,7 +47,7 @@ export default {
     };
   },
   methods: {
-    ...mapActions(['fetchUserBlacklist', 'fetchShippertBlacklist', 'deleteUserBlacklistItem', 'createUserBlacklistItem']),
+    ...mapActions(['fetchUserBlacklist', 'fetchShippertBlacklist', 'deleteUserBlacklistItem', 'createUserBlacklistItem', 'createNotification']),
     setCurrentTab(event) {
       if(!event.target.closest('button')) { return; }
       this.currentTab = event.target.dataset.tab;
@@ -58,8 +58,28 @@ export default {
     createItem(event) {
       const input = event.target.closest('.add-blacklist-item').querySelector('#new-blacklist-item');
       const newItem = input.value;
+      if(!this.isValidUrlParameter(newItem)) {
+        this.createNotification({content: "Name of blacklist item isn't allowed", type: `warning`});
+        return;
+      }
+      if(newItem.length > 20) {
+        this.createNotification({content: "Item name cannot exceed more than 20 characters!", type: `warning`});
+        return;
+      }
       this.createUserBlacklistItem(newItem);
       input.value = "";
+    },
+    isValidUrlParameter(value) {
+      if (value === undefined || value === null) {
+        return false;
+      }
+      if (typeof value !== 'string') {
+        return false;
+      }
+      if (value.trim() === '') {
+        return false;
+      }
+      return !(/[^a-zA-Z0-9_\.~!\*'\(\);:@&=\+\$,\/\?%\[\]#-]/.test(value));
     }
   },
   computed: {
